548b82e3af3da6635389aa42fa298ac71a2fcb39
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-18 15:51:54 -0500

    Add files via upload

---
+++ plugins/apkmod.js
@@ -0,0 +1,44 @@
+//jangan ubah om plisss
+//om jangan ambil
+//yah om
+let handler = async m => m.reply(`
+〘 APK LIST 𝗠𝗢𝗗 〙
+❃❃❃❃❃❃❃❃❃❃❃❃❃❃❃
+      Samu330:) *01* 
+❃❃❃❃❃❃❃❃❃❃❃❃❃❃❃
+%readmore
+➨V2rayNG mod
+➢https://drive.google.com/uc?export=download&id=1-DPC905vatO9I0M9AOdQJwHWnchQev_8
+%readmore
+➨Sockshttp mod
+➢https://drive.google.com/uc?export=download&id=1-0dJwiebqLgGOZyGO4b6Jx7tYlLdotmg
+%readmore
+➨Clash For Android mod
+➢https://drive.google.com/uc?export=download&id=1-3ZvuktNFoip_ap3WZ2rZTQoMUHE4H3S
+%readmore
+➨HTTP INYECTOR MOD
+➢https://drive.google.com/uc?export=download&id=1-2KwU_HytP-CSUTA419i23SVBsUEPgiH
+%readmore
+➨𝗔𝗹𝗶𝗴𝗵𝘁 𝗠𝗼𝘁𝗶𝗼𝗻 𝗺𝗼𝗱
+➢https://apkdone.com/alight-motion/'
+%readmore
+➨𝗞𝗶𝗻𝗲𝗺𝗮𝘀𝘁𝗲𝗿
+➢https://apkdone.com/kinemaster-apk-mods/
+%readmore
+➨𝗜𝗻𝘀𝗵𝗼𝘁
+➢https://apkdone.com/inshot/
+%readmore
+➨𝗙𝗶𝗹𝗺𝗼𝗿𝗮𝗚𝗼
+➢https://apkdone.com/filmorago/
+%readmore
+➨𝗟𝗶𝗴𝗵𝘁𝗿𝗼𝗼𝗺
+➢https://apkdone.com/adobe-lightroom/
+%readmore
+➨𝗣𝗶𝗰𝘀𝗮𝗿𝘁
+➢https://apkdone.com/picsart-apk-mods/
+`.trim()) 
+handler.help = ['apkmod']
+handler.tags = ['tools']
+handler.command = /^apkmod|aplikasimod$/i
+
+module.exports = handler
