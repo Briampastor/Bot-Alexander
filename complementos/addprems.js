82690311d0a671ff568cc000224c0b86875c2ce3
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-18 07:51:35 -0500

    Update and rename plugins/addprems.js to complementos/addprems.js

--- complementos/addprems.js
+++ complementos/addprems.js
@@ -4,11 +4,11 @@ let handler = async(m, { conn, text }) => {
 let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw 'Tag salah satu lah,dan masukkan nomor untuk di verivikasi !'
-  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
+  if ( ! who ) throw  'Etiqueta uno,e ingrese el número para verificación !'
+  // si (participantes.map (v => v.jid) .includes (global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].premium = true
-  var nomor = m.sender
-    m.reply(`*Done berhasil added User✅*\n\n*Nomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Expired:* 30Days\n*Thanks For Added Premium !*`)
+  var  nomor  =  m . remitente
+    m . respondedor ( `* Listo usuario agregado con exito✅ * \ n \ n * Nombre: wa.me / $ {nomor. split (" @ s.whatsapp.net ") [0]} \ n * Caducado: * 30Días \ n * ¡Gracias por agregar Premium! * ` )
   // } else m.reply('Ada nomor host disini...')
 }
 handler.help = ['addprems <nomor>']
