1ba1211f87f5a0cd0987fb21824491370156e616
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-19 21:51:35 -0500

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -2,16 +2,16 @@ let { Presence } = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
 
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
-	if (!args || !args[0]) return conn.reply(m.chat, `Formato incorrecto!\n\n*Ejemplpo* : _${usedPrefix + command} Hola simi_`, m)
+	if (!args || !args[0]) return conn.reply(m.chat, `Formato incorrecto!\n\n*Ejemplpo* : _${usedPrefix + command} hola_`, m)
 	let text = args.join` `
 	fetch("https://api.simsimi.net/v1/?text=" + encodeURIComponent(text) + "&lang=es")
   .then(res => res.json())
   .then(batch => {
     conn.updatePresence(m.chat, Presence.composing)
   conn.reply(m.chat, `${batch.success}`, m)
-  }) .catch(() => { conn.reply(m.chat, `_¡La función simi es errónea! Perdón :(_`, m) })
+  }) .catch(() => { conn.reply(m.chat, `_¡La función arrow está desabilitada!_`, m) })
 }
-handler.help = ['simi','s'].map(v => v + ' *text*')
+handler.help = ['simi','arrow|walle'].map(v => v + ' *text*')
 handler.tags = ['fun']
 handler.command = /^(simi|arrow|walle)$/i
 handler.owner = false
