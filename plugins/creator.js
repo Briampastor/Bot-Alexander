550955e5adbf09de3fc00d6ffea68777f99ab77b
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-20 01:47:01 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '593 98 763 4004', 'Arrow', m)
+  // this.sendContact(m.chat, '593987634004', 'Arrow', m)
   this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
 }
 handler.help = ['owner', 'creator']
