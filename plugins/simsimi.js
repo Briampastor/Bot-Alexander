30e182bdda593f93b048bc70d2dbccf3c3fee46f
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-20 03:28:48 -0500

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -9,7 +9,7 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
   .then(batch => {
     conn.updatePresence(m.chat, Presence.composing)
   conn.reply(m.chat, `${batch.success}`, m)
-  }) .catch(() => { conn.reply(m.chat, `_¡La función arrow está desabilitada!_`, m) })
+  }) .catch(() => { conn.reply(m.chat, `_¡La función simi está desabilitada!_`, m) })
 }
 handler.help = ['simi','arrow|walle'].map(v => v + ' *text*')
 handler.tags = ['fun']
