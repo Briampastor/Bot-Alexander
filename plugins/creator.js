03f0e2052cf5b1317dda8a38431ee56477111b03
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-20 04:25:03 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,11 @@
 let handler = function (m) {
+  {m.reply('*Hola👋 este es mi creador ⚡Arrrow_OG⚡*')}
   // this.sendContact(m.chat, '593987634004', 'Arrow', m)
   this.sendContact(m.chat, '593960178180', 'Arrow', m)
 }
-handler.help = ['owner', 'creator']
+handler.help = ['owner', 'creador']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+handler.command = /^(owner|creador)$/i
 
 module.exports = handler
