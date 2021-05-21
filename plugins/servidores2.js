74fadb6a3d6dbfdd6be9e8a7d1b171a8806d2dea
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-20 22:19:29 -0500

    Add files via upload

---
+++ plugins/servidores2.js
@@ -0,0 +1,45 @@
+//jangan ubah om plisss
+//om jangan ambil
+//yah om
+{await m.reply('Buscando🔎')}
+let handler = async m => m.reply(`
+〘 LISTA DE SERVIDORES SOCKSHTTP〙
+❃❃❃❃❃❃❃❃❃❃❃❃❃❃❃
+      Arrow_OG *01* 
+❃❃❃❃❃❃❃❃❃❃❃❃❃❃❃
+%readmore
+➨ᴀғ❤️ꦿᴬʳʳᵒʷ ⚡ MOVISTAR-COMBOS-WHATSAPP-PLAY 30 DIAS⚡
+➢https://drive.google.com/uc?export=download&id=1-28edINmFP5qZiMGMhaxNNeJjohzchMG
+%readmore
+➨ᴀғ❤️ꦿᴬʳʳᵒʷ ⚡ TUENT COMBOS 30 DIAS⚡
+➢https://drive.google.com/uc?export=download&id=1-34XUH5OGmpOq9V9eH6rHS43nJ3P-TsO
+%readmore
+➨ᴀғ❤️ꦿᴬʳʳᵒʷ ⚡#2 MOVISTAR-COMBOS-WHATSAPP-PLAY 30 DIAS⚡
+➢https://drive.google.com/uc?export=download&id=1-9EGe-W9HAVS-DiQTW329P_TZwsKwlqp
+%readmore
+➨ᴀғ❤️ꦿᴬʳʳᵒʷ ⚡#2 TUENTI COMBOS 30 DIAS⚡
+➢https://drive.google.com/uc?export=download&id=1-3fPNa2YA7V3g6En0KO73Vrgl2f-Ekn8
+%readmore
+➨ᴀғ❤️ꦿᴬʳʳᵒʷ ⚡CNT 3Y6$ #2 30 DIAS⚡
+➢https://drive.google.com/uc?export=download&id=1-I1eQ-XfCX6IA_pn3cr1j4PeHajZ_Hbp
+%readmore
+➨ᴀғ❤️ꦿᴬʳʳᵒʷ ⚡CNT 3Y6$ 30 DIAS⚡
+➢https://drive.google.com/uc?export=download&id=1-O_bAEMTgOkBDcD-9s49mMKC3utSB3c1
+%readmore
+➨ᴀғ❤️ꦿᴬʳʳᵒʷ ⚡CNT 10$ Antibaneo 30 DIAS⚡
+➢https://drive.google.com/uc?export=download&id=1-KVlYqUCEjNL4a8d3oI1zBDfWYqxAXbg
+%readmore
+➨ᴀғ❤️ꦿᴬʳʳᵒʷ⚡ CLARO-COMBOS-PLAN-FACEBOOK 30 DIAS⚡
+➢https://drive.google.com/uc?export=download&id=1-YN3htISc8SNQTi6hxhv_3jOqFsKj6E7
+%readmore
+➨ᴀғ❤️ꦿᴬʳʳᵒʷ⚡ TUENTI SPOTIFY 30 DIAS⚡
+➢https://drive.google.com/uc?export=download&id=1-Oh3ltWlMOJucohe6XMRoJMYiJba6kQ-
+%readmore
+➨ᴀғ❤️ꦿᴬʳʳᵒʷ⚡#2 CLARO-COMBOS-PLAN-FACEBOOK 30 DIAS⚡
+➢https://drive.google.com/uc?export=download&id=1-b6Kk7QRlkVQfWDhOBpwJB7ZFXFgu03L
+`.trim()) 
+handler.help = ['servidores2|servers']
+handler.tags = ['tools']
+handler.command = /^servidores2|servers|aplikasimod$/i
+
+module.exports = handler
