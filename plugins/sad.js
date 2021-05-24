6679953bfdaef83189039ac3fdf2aae8a079d2cb
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-24 04:04:40 -0500

    Add files via upload

---
+++ plugins/sad.js
@@ -0,0 +1,17 @@
+let handler  = async (m, { conn }) => {
+       conn.sendFile(m.chat, 'sad.m4a', '', 'xd', m)
+}
+handler.help = ['sad']
+handler.tags = ['premium']
+handler.command = /^(sad)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = true
+handler.private = false
+handler.admin = true
+handler.botAdmin = false
+handler.fail = null
+handler.limit = false
+handler.exp = 100
+module.exports = handler
