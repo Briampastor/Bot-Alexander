09d49e630f2017e6fec8af0506600b2a20fc2bdc
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-24 04:22:45 -0500

    Add files via upload

---
+++ plugins/idea.js
@@ -0,0 +1,17 @@
+let handler  = async (m, { conn }) => {
+       conn.sendFile(m.chat, 'idea.m4a', '', 'xd', m)
+}
+handler.help = ['idea']
+handler.tags = ['premium']
+handler.command = /^(idea)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = true
+handler.private = false
+handler.admin = true
+handler.botAdmin = false
+handler.fail = null
+handler.limit = false
+handler.exp = 100
+module.exports = handler
