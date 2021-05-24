225bbd20544c5ba0492f16a7083b21964743a0cb
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-24 03:34:30 -0500

    Add files via upload

---
+++ plugins/piensa.js
@@ -0,0 +1,17 @@
+let handler  = async (m, { conn }) => {
+       conn.sendFile(m.chat, 'piensa.m4a', '', 'xd', m)
+}
+handler.help = ['piensa']
+handler.tags = ['premium']
+handler.command = /^(piensa)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = true
+handler.private = false
+handler.admin = true
+handler.botAdmin = false
+handler.fail = null
+handler.limit = false
+handler.exp = 100
+module.exports = handler
