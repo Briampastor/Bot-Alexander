576922a697a791b66d6716e4661f0d5078697a0d
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-25 01:40:17 -0500

    Add files via upload

---
+++ plugins/addpremium.js
@@ -0,0 +1,34 @@
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async(m, { conn, text }) => {
+let who
+  if (m.isGroup) who = m.mentionedJid[0]
+  else who = m.chat
+  if (!who) throw 'Etiquete a uno, e ingrese el número para verificación!'
+  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
+    global.DATABASE._data.chats[m.chat].premium = true
+  var nomor = m.sender
+  
+  {
+      conn.sendFile(m.chat, 'Bot-Arrow10.png', '', text.trim(), m)
+}
+  
+    m.reply(`*Listo usuario agregado como premium✅*\n\n*Numero: wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*caduca en:* 30Dias\n*Gracias por agregar Premium !*`)
+  
+{
+
+      conn.sendFile(m.chat, 'Vos del bot.m4a', '', 'xd', m)
+
+}
+  
+}
+handler.help = ['addpremium Nombre']
+handler.tags = ['owner']
+handler.command = /^addpremium$/i
+handler.rowner = true
+
+
+
+
+
+module.exports = handler
