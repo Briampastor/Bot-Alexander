67b15f582aeef849f325b32b58abd57bbe8c2cc5
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-26 02:55:47 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -187,8 +187,8 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 600) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 100) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
 }
