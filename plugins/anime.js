8c2b55ed87e9b8c04eda741f5b99b9312b76a5d8
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-27 03:52:47 -0500

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -3,14 +3,14 @@ let fetch = require('node-fetch')
 let handler = async(m, { conn, args, usedPrefix }) => {
     if (args.length == 0) return conn.reply(m.chat, `Usar ${usedPrefix}anime\nPor favor escribe: ${usedPrefix}anime [consulta]\nEjemplo: ${usedPrefix}anime random\n\nconsulta que esta disponible:\nrandom, waifu, husbu, neko`, m)
     if (args[0] == 'random' || args[0] == 'waifu' || args[0] == 'husbu' || args[0] == 'neko') {
-  await m.reply('*buscando...*')
+  await m.reply('*buscando...🔎*')
 
         fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/' + args[0] + '.txt')
             .then(res => res.text())
             .then(body => {
                 let randomnime = body.split('\n')
                 let randomnimex = randomnime[Math.floor(Math.random() * randomnime.length)]
-                conn.sendFile(m.chat, randomnimex, '', 'Dasar Wibu', m)
+                conn.sendFile(m.chat, randomnimex, '', 'Arrow_OG', m)
             })
             .catch(() => {
                 conn.reply(m.chat, 'Hay un error cuy... Puede preguntar ke\n*Instagram:* @Arrow11012020!', m)
@@ -29,6 +29,7 @@ handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
+handler.registrar = true
 
 handler.admin = false
 handler.botAdmin = false
