43ca38c036171c13d862715c03a5f5b394caa839
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-27 04:55:52 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -289,7 +289,7 @@ module.exports = {
               pp = await this.getProfilePicture(user)
             } catch (e) {
             } finally {
-              text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Bienvenido/✨a, @user!').replace('@subject', this.getName(jid)) :
+              text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || '✨Bienvenido/a✨, @user!').replace('@subject', this.getName(jid)) :
                 (chat.sBye || this.bye || conn.bye || 'Adios👋 pavo, @user!')).replace('@user', '@' + user.split('@')[0])
               this.sendFile(jid, pp, 'pp.jpg', text, null, false, {
                 contextInfo: {
