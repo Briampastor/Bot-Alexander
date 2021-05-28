532fa6524dfd2d9e43566483b9b000a451be6372
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-27 22:42:40 -0500

    Add files via upload

---
+++ plugins/Bot.js
@@ -0,0 +1,6 @@
+let handler = m => m.reply('*Si... si estoy aqui.*👻\n\n¿En que te puedo ayudar pavo/a🦃? ✴️Escribe *#help* o *#Menú* para que puedas ver mis funciones✴️')
+
+handler.customPrefix = /Bot|kuhong|hong|Hong/i
+handler.command = new RegExp
+
+module.exports = handler
