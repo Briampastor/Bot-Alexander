cd6ce97c63530ed22d42ce27c36f469cd9bccb50
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-27 22:44:30 -0500

    Update Bot.js

--- plugins/Bot.js
+++ plugins/Bot.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('*Si... si estoy aqui.*👻\n\n¿En que te puedo ayudar pavo/a🦃? ✴️Escribe *#help* o *#Menú* para que puedas ver mis funciones✴️')
+let handler = m => m.reply('*Si... si estoy aqui.*👻\n\n¿En que te puedo ayudar pavo/a🦃? Escribe *#help* o *#Menú* para que puedas ver mis funciones.')
 
 handler.customPrefix = /Bot|kuhong|hong|Hong/i
 handler.command = new RegExp
