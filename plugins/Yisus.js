ffacb2c8990194acfe507dee11e3c4e4a28f1bce
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-28 00:44:38 -0500

    Add files via upload

---
+++ plugins/Yisus.js
@@ -0,0 +1,6 @@
+let handler = m => m.reply('*Es una...\n\nloca que le encantan los hombres de color afrodescendiente*')
+
+handler.customPrefix = /Yisus|yisus|yisuss|Hong/i
+handler.command = new RegExp
+
+module.exports = handler
