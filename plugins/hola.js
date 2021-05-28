3460a0b04c5a6296acb98494fcc1cd84213da50e
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-27 22:14:30 -0500

    Update hola.js

--- plugins/hola.js
+++ plugins/hola.js
@@ -1,6 +1,6 @@
 let handler = m => m.reply('Si. si estoy aqui....\n\nHola👋 pavo/a✨')
 
-handler.customPrefix = Hola/i
+handler.customPrefix = Hola
 handler.command = new RegExp
 
 module.exports = handler
