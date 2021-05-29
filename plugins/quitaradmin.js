faf8d3b66331c621d79c7080ceb466e68c44e73c
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-29 02:50:24 -0500

    Update quitaradmin.js

--- plugins/quitaradmin.js
+++ plugins/quitaradmin.js
@@ -1,6 +1,11 @@
 let handler = async (m, { conn, args }) => {
   let users = m.mentionedJid
   conn.groupDemoteAdmin(m.chat, users)
+{
+
+      conn.sendFile(m.chat, 'Vos del bot.m4a', '', 'xd', m)
+
+}
 }
 handler.help = ['quitaradmin','member','v'].map(v => 'o' + v + ' @user')
 handler.tags = ['owner']
