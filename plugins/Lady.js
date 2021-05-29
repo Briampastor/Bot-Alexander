ad1db8572fe7fcf460f15c996014632cb90796dc
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-29 01:32:58 -0500

    Add files via upload

---
+++ plugins/Lady.js
@@ -0,0 +1,6 @@
+let handler = m => m.reply('*Quien es lady??*\n\n*✨Lady ✨es el amor❤️ de la vida de Arrow... su pava🦃, su amiga👻, su esposa👩‍❤️‍👨 su todo que siempre esta a su lado🌹 y esta desde hace ya muchos años 🔥siempre el la cuida a ella y ella a el , estan muy enamorados 😍y ese amor tan grande durará toda la vida...✨ Te amo sol😘❣️*')
+
+handler.customPrefix = /Lady|lady|ladysita|Hong/i
+handler.command = new RegExp
+
+module.exports = handler
