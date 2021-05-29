1f95c99848dd646be3c05de1a5dfb487382ed359
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-29 01:46:55 -0500

    Add files via upload

---
+++ plugins/hentai.js
@@ -0,0 +1,36 @@
+let imageToBase64 = require('image-to-base64');
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+  await m.reply('_*Espera estoy buscando...🔎*_')
+let str = `
+TOBAT BODO
+`.trim()
+
+    axios.get('http://api-melodicxt-2.herokuapp.com/api/random/hentai?apiKey=APIKEY')
+    .then((res) => {
+      imageToBase64(res.data.result.result)
+        .then(
+          (ress) => {
+            let buf = Buffer.from(ress, 'base64')
+
+     conn.sendFile(m.chat, buf, 'foto.jpg', str, m)
+        })
+    })
+}
+handler.help = ['hentai']
+handler.tags = ['premium']
+handler.command = /^(hentai)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = false
+handler.private = true
+handler.registrar = true
+
+handler.admin = true
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = 10
+
+module.exports = handler
