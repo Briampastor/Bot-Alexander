a3a0843152fadb6e92643fd842a958312b2b7e9c
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-29 03:12:06 -0500

    Update quitaradmin.js

--- plugins/quitaradmin.js
+++ plugins/quitaradmin.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, args }) => {
-  let users = m.mentionedJid
   let pp = './src/avatar_contact.png'
+  let users = m.mentionedJid
+  
   conn.groupDemoteAdmin(m.chat, users)
 
 {m.reply('*Un nuevo admin se agrego con exito✅*')}
