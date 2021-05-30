a38629d1d8c90e7006f7925548af27fd6bcca087
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-30 07:04:05 -0500

    Update RsptServidor.js

--- plugins/RsptServidor.js
+++ plugins/RsptServidor.js
@@ -1,6 +1,6 @@
 let handler = m => m.reply('*Hola que tal tu dia??*\n\n*Buscas servidores de Tuenti,Claro,Cnt o Movistar...??? debes poner el comando #servers o #servers2 y enviare los servidores*')
 
-handler.customPrefix = /Servidores|servidor|server|talleriame/i
+handler.customPrefix = /Servidores|servidor|tcl|talleriame/i
 handler.command = new RegExp
 
 module.exports = handler
