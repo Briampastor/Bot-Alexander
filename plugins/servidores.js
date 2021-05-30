7e54632b35c48d24bc19141b7e920ce1940a8792
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-29 23:10:02 -0500

    Update servidores.js

--- plugins/servidores.js
+++ plugins/servidores.js
@@ -37,9 +37,9 @@ let handler = async m => m.reply(`
 ➨ᴀғ❤️ꦿᴬʳʳᵒʷ⚡#2 CLARO-COMBOS-PLAN-FACEBOOK 30 DIAS⚡
 ➢https://drive.google.com/uc?export=download&id=1-b6Kk7QRlkVQfWDhOBpwJB7ZFXFgu03L
 `.trim()) 
-handler.help = ['servers1']
+handler.help = ['servers']
 handler.tags = ['tools']
 handler.registrar = true
-handler.command = /^servers1|aplikasimod$/i
+handler.command = /^servers|server aplikasimod$/i
 
 module.exports = handler
