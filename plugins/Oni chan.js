cff21a19a8aa3ba308eb5b9581a2b97fb6dee16b
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-31 05:13:09 -0500

    Update Oni chan.js

--- plugins/Oni chan.js
+++ plugins/Oni chan.js
@@ -2,7 +2,7 @@ let handler = m => {
       conn.sendFile(m.chat, 'resultado1.mp3', '', 'xd', m)
 }
 
-handler.customPrefix = /Servidores|servidor|server|servers/i
+handler.customPrefix = /Onichan|oni chan|Oni chan|Oni Chan/i
 handler.command = new RegExp
 
 module.exports = handler
