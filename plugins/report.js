1363945c32ff75102a6fa3a633ab97f3c5aa0176
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-31 02:16:11 -0500

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -2,12 +2,12 @@
 
 let handler = async(m, { conn, text }) => {
     if (!text) throw 'Silahkan masukkan laporan'
-    if (text.length > 300) throw 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks!'
-    const laporan = `*「 REPORT 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${text}`
+    if (text.length > 300) throw '*X* _Lo siento texto demasiado largo_ *X*, ✴️_Máximo 300 texto_✴️!'
+    const laporan = `*「 REPORTE 」*\n*✨Nombre✨:* wa.me/${m.sender.split`@`[0]}\n*Mensaje📬:* ${text}`
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '593987634004@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender) // Mwehehehehe
-    m.reply('El problema ha sido informado al propietario con éxito👌,informes falsos no seran respondido a!')
+    m.reply('*El problema ha sido informado al propietario con éxito✅,informes falsos no seran respondido a!')
 }
 handler.help = ['bug', 'report'].map(v => v + ' <laporan>')
 handler.tags = ['info']
