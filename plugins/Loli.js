8d1173dab9a5aee33c1f4c954ae85571ce0b0bb7
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-31 05:57:11 -0500

    Add files via upload

---
+++ plugins/Loli.js
@@ -0,0 +1,8 @@
+let handler = m => {
+      conn.sendFile(m.chat, 'Loli.mp4', '', 'xd', m)
+}
+
+handler.customPrefix = /Loli|loli|lolis|Lolis/i
+handler.command = new RegExp
+
+module.exports = handler
