313960f003d79071ade1ebbd6e295db6f0172df9
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-31 06:08:23 -0500

    Delete Loli.js

--- plugins/Loli.js
+++
@@ -1,8 +0,0 @@
-let handler = m => {
-      conn.sendFile(m.chat, 'Loli.mp4', '', 'xd', m)
-}
-
-handler.customPrefix = /Loli|loli|lolis|Lolis/i
-handler.command = new RegExp
-
-module.exports = handler
