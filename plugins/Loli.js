01424c257a98921c399e1f275f82dc6ae6f5bd76
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-31 05:29:06 -0500

    Update Loli.js

--- plugins/Loli.js
+++ plugins/Loli.js
@@ -2,6 +2,8 @@ let handler = m => {
       conn.sendFile(m.chat, 'Loli.mp4', '', 'xd', m)
 }
 
+{await m.reply('*⏳Espera⌛*')}
+
 handler.customPrefix = /Loli|loli|lolis|Lolis/i
 handler.command = new RegExp
 
