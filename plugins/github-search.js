cc79264555bd84363a45b92cc2a8e462a913cc8a
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-31 01:07:37 -0500

    Update github-search.js

--- plugins/github-search.js
+++ plugins/github-search.js
@@ -22,6 +22,8 @@ ${repo.open_issues} Issue${repo.description ? `
 }
 handler.help = ['githubsearch'].map(v => v + ' <pencarian>')
 handler.tags = ['tools']
+handler.registrar = true
+handler.premium = true
 
 handler.command = /^g(ithub|h)search$/i
 
