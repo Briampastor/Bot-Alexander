2db25d07a45e9b0dc1f1b78e6e1ae65ab2535a36
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-01 05:24:25 -0500

    Delete botchat.js

--- plugins/botchat.js
+++
@@ -1,6 +0,0 @@
-let handler = m => m.reply('Si, estoy aqui??\n\nQuiere iniciar un bot? Escriba! Ayuda o !menu yaa ;)')
-
-handler.customPrefix = /Kuhong|kuhong|hong|Hong/i
-handler.command = new RegExp
-
-module.exports = handler
