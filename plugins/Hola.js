881bc5fc82bab2d1b7821e8d38ca40147c60f2a2
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-05-31 23:36:31 -0500

    Update Hola.js

--- plugins/Hola.js
+++ plugins/Hola.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('*Hola👻*\n\n¿En que te puedo ayudar pavo/a🦃? Escribe *#help* o *#Menú* para que puedas ver mis funciones.')
+let handler = m => m.reply('*Hola soy un bot🌚*\n\n¿En que te puedo ayudar pavo/a🦃? Escribe *#help* o *#Menú* para que puedas ver mis funciones.')
 
 handler.customPrefix = /Hola|hola|Oa|Hong/i
 handler.command = new RegExp
