8c99dcd6cee1a3a7280e8c2ead2dcac746d1adb1
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-01 18:25:21 -0500

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -2,14 +2,14 @@ let handler = function (m) {
   if (!m.quoted) throw false
   let { fromMe, id, isBaileys } = m.quoted
   if (!fromMe) throw false
-  if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
+  if (!isBaileys) throw 'El mensaje no fue enviado por bot!'
   this.deleteMessage(m.chat, {
     fromMe,
     id,
     remoteJid: m.chat
   })
 }
-handler.help = ['del', 'delete']
+handler.help = ['del', 'delete','borrar']
 handler.tags = ['info']
 
 handler.command = /^del(ete)?$/i
