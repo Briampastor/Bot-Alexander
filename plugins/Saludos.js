8e18013b43513fa204009f2f541d1e9aa2e06076
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-01 20:59:15 -0500

    Add files via upload

---
+++ plugins/Saludos.js
@@ -0,0 +1,7 @@
+let handler = m => m.reply('*Holaque tal como va tu dia ??*\n\n*Que tengas un exelente dia,tarde o noche... y si vas a dormir descansa mañana es un nuevo inicio*')
+
+handler.customPrefix = /días|dias|tardes|Tardes|Dias|Días||dia|Dia|Día|Noches|noches/i
+handler.command = new RegExp
+handler.private = false
+
+module.exports = handler
