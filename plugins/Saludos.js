3d5c3a39c12908ed7096f8b7631358eff61db8e2
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-01 21:18:04 -0500

    Update Saludos.js

--- plugins/Saludos.js
+++ plugins/Saludos.js
@@ -1,6 +1,6 @@
 let handler = m => m.reply('*Hola👋 que tal como va tu dia ??*\n\n*Que tengas un exelente dia,tarde o noche... y si vas a dormir descansa mañana es un nuevo inicio🌚*')
 
-handler.customPrefix = /buenos días|Buenos dias|buenas tardes|Buenas Tardes|Buenos Dias|buenos Días||buen dia|Buen dia|buen día|Buenas noches|buenas noches|Buenas|hasta mañana|Hasta mañana|Buen/i
+handler.customPrefix = /buenos días|Buenos dias|buenas tardes|Buenas Tardes|Buenos Dias|buenos Días||buen dia|Buen dia|buen día|Buenas noches|Hasta mañana|buenas noches|Buenas|hasta mañana|Hasta mañana|Buen/i
 handler.command = new RegExp
 handler.private = false
 handler.group = true
