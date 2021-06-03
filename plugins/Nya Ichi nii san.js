d9b4cdedbe2a9833fbc6847717cc814144889d71
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-02 20:40:23 -0500

    Update Nya Ichi nii san.js

--- plugins/Nya Ichi nii san.js
+++ plugins/Nya Ichi nii san.js
@@ -1,5 +1,5 @@
 let handler = m => {
-      conn.sendFile(m.chat, 'Nya Ichi nii san.mp4', '', 'xd', m)
+      conn.sendFile(m.chat, 'Nya Ichi nii san.mp4', '', '✨𝐁𝐨𝐭 𝐀𝐫𝐫𝐨𝐰✨', m)
 }
 
 handler.customPrefix = /Ichinisan|ichinisian|ichinisan|Nya/i
