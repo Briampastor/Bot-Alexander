fa9950ecae7260697fdf4c056fc030fecb49c225
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-02 20:28:40 -0500

    Add files via upload

---
+++ plugins/Editado#1.js
@@ -0,0 +1,12 @@
+let handler = m =>{
+
+      conn.sendFile(m.chat, 'Editado#1.mp4', '', 'xd', m)
+
+}
+
+handler.customPrefix = /Editado1|editado1/i
+handler.command = new RegExp
+handler.private = false
+handler.group = true
+
+module.exports = handler
