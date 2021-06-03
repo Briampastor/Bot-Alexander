149a555cf94c931ab95047f1a47b840e876969ea
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-03 01:30:54 -0500

    Update dadu.js

--- plugins/dadu.js
+++ plugins/dadu.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, command, text }) => {
   conn.reply(m.chat, `
-Angka Dadu Anda : ${pickRandom(['1','2','3','4','5','6','7','8','9','10','11','12'])}
-Angka Dadu Bot : ${pickRandom(['1','2','3','4','5','6','7','8','9','10','11','12'])}
+Tu número de dados : ${pickRandom(['1','2','3','4','5','6','7','8','9','10','11','12'])}
+Números de dados de bot : ${pickRandom(['1','2','3','4','5','6','7','8','9','10','11','12'])}
 
-Siapakah pemenangnya?
+quien es el ganador?
 `.trim(), m)
 }
-handler.help = ['dadu']
+handler.help = ['dado']
 handler.tags = ['game']
-handler.command = /^dadu/i
+handler.command = /^dado/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
