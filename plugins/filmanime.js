b092583bdcf55c942271eb708b9030e42ddfe9bd
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-03 01:12:21 -0500

    Update filmanime.js

--- plugins/filmanime.js
+++ plugins/filmanime.js
@@ -1,17 +1,17 @@
 let axios = require("axios");
 let handler = async(m, { conn, text }) => {
 
-    if (!text) return conn.reply(m.chat, 'Harap Masukan Nama Film Animenya', m)
+    if (!text) return conn.reply(m.chat, 'Ingresa el nombre de la película de anime.', m)
 
 	axios.get(`https://arugaz.herokuapp.com/api/sdmovie?film=${text}`).then ((res) => {
 	 	let hasil = `*Película ${text}*\n\nTítulo : ${res.data.result.title}\nClasificación : ${res.data.result.rating}\nInformacion : ${res.data.result.sinopsis}\nLink Video : ${res.data.result.video}`
 
-    conn.reply(m.chat, hasil, m)
+    conn.reply(m.chat, resultado, m)
 	})
 }
-handler.help = ['nombre de pelicula'].map(v => v + ' Película')
+handler.help = ['nombre del anime'].map(v => v + ' Película')
 handler.tags = ['tools']
-handler.command = /^(buscarpeli)$/i
+handler.command = /^(buscaranime)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
