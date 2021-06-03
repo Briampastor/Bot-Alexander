fbe33f08c8b9d677ef06f69e18d184607fb4b9a2
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-03 02:14:21 -0500

    Update perfil.js

--- plugins/perfil.js
+++ plugins/perfil.js
@@ -13,9 +13,9 @@ let handler = async (m, { conn }) => {
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
     let str = `
-*🔥Nombre🔥* : ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\nAbout: ' + about : ''}
+*🔥Nombre🔥* : ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\n*Descripción:* ' + about : ''}
 *✨Número✨* : ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-*💢Enlace💢* : https://wa.me/${who.split`@`[0]}${registered ? '\nEdad: ' + age : ''}
+*💢Enlace💢* : https://wa.me/${who.split`@`[0]}${registered ? '\n*✨Edad✨:* ' + age : ''}
 *XP* : TOTAL ${exp} (${exp - min} / ${xp}) [${max - exp} left to levelup]
 *Nivel🎚️* : ${level}
 *Límite*: ${limit}
