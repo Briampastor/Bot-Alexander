7c4db4e750cb3c7420cc51828ca43e134fa58c7a
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-02 20:13:37 -0500

    Update Editado#1.js

--- plugins/Editado#1.js
+++ plugins/Editado#1.js
@@ -2,7 +2,7 @@ let handler = m => {
       conn.sendFile(m.chat, 'media/ 'Editado#1.mp4', '', 'xd', m)
 }
 
-handler.customPrefix = /Editado1|editado1/i
+handler.customPrefix = /Edit1|editado1/i
 handler.command = new RegExp
 handler.private = false
 handler.group = true
