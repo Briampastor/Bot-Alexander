de74b0d84f76e5b3b03e9c68540a2ad712752220
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-02 20:33:04 -0500

    Update Oni chan.js

--- plugins/Oni chan.js
+++ plugins/Oni chan.js
@@ -1,5 +1,5 @@
 let handler = m => {
-      conn.sendFile(m.chat, 'Oni chan.m4a', '', 'xd', m)
+      conn.sendFile(m.chat, 'Oni chan.m4a', '', '✨𝐁𝐨𝐭 𝐀𝐫𝐫𝐨𝐰✨', m)
 }
 
 handler.customPrefix = /Onichan|oni chan|Oni chan|Oni Chan/i
