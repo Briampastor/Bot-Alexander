f63cfd22b691c1ab4a9d6af2aa279abfa0775234
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-04 21:42:49 -0500

    Update buylimit.js

--- plugins/buylimit.js
+++ plugins/buylimit.js
@@ -1,7 +1,7 @@
 const xpperlimit = 350 
 let handler = async (m, { conn, command, args }) => {
-  let count = command.replace(/^buy/i, '')
-  count = count ? /all/i.test(count) ? Math.floor(global.DATABASE._data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
+  let count = command.replace(/^comprar/i, '')
+  count = count ? /todo/i.test(count) ? Math.floor(global.DATABASE._data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
   if (global.DATABASE._data.users[m.sender].exp >= xpperlimit * count) {
     global.DATABASE._data.users[m.sender].exp -= xpperlimit * count
@@ -9,9 +9,9 @@ let handler = async (m, { conn, command, args }) => {
     conn.reply(m.chat, `-${xpperlimit * count} XP\n+ ${count} Limit`, m)
   } else conn.reply(m.chat, `*XP no es suficiente para comprar* ${count} limit`, m)
 }
-handler.help = ['comprar cantidad límite', 'comprar cantidad límite', 'buyall']
+handler.help = ['comprar cantidad límite', 'comprar cantidad límite', 'comprartodo']
 handler.tags = ['xp']
-handler.command = /^comprar([0-9]+)Comprar|buyall$/i
+handler.command = /^comprar([0-9]+)comprar|comprartodo$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
