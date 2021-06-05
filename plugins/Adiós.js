c7e97d9ffc5b2af8dd29ebf352e5ee4d4fd9cd3b
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-05 02:41:46 -0500

    Update Adiós.js

--- plugins/Adiós.js
+++ plugins/Adiós.js
@@ -1,4 +1,5 @@
 let handler = m => m.reply('*Adiós👋* %name, *✨Que descanses✨*')
+// if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
 const { MessageType } = require('@adiwajshing/baileys')
 handler.customPrefix = /Adiós|Adios|adios/i
 handler.command = new RegExp
