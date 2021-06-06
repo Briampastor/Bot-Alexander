139ea2e1e0ea53c04457aab19ce7d92e068a9e4a
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-06 03:52:22 -0500

    Add files via upload

---
+++ plugins/ArrowYArrowna#1.js
@@ -0,0 +1,17 @@
+let handler  = async (m, { conn }) => {
+       conn.sendFile(m.chat, 'ArrowYArrowna#1.mp4', '', '✨𝐁𝐨𝐭 𝐀𝐫𝐫𝐨𝐰✨', m)
+}
+handler.help = ['AyA1']
+handler.tags = ['premium']
+handler.command = /^(AyA1)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = true
+handler.botAdmin = false
+handler.fail = null
+handler.limit = false
+handler.exp = 100
+module.exports = handler
