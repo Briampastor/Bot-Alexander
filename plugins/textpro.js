ea0d6f89394e6b844d163f1d3d7ff26c751d021e
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-06 03:08:29 -0500

    Update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args: [effect], text: txt }) => {
     throw e
   }
 }
-handler.help = ['textpro'].map(v => v + ' <effect> <text>|[text2]')
+handler.help = ['textpro2'].map(v => v + ' <effect> <text>|[text2]')
 handler.tags = ['tools']
 handler.command = /^(textpro)$/i
 
