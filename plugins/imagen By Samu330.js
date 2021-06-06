9492ec0bed505c28ca4ff640f0935973bb5abb8e
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-06 05:44:39 -0500

    Update imagen By Samu330.js

--- plugins/imagen By Samu330.js
+++ plugins/imagen By Samu330.js
@@ -8,8 +8,8 @@ let url = await fetch('https://api.fdci.se/sosmed/rep.php?gambar=' + encodeURICo
 //let url = await fetch('https://api.fdci.se/sosmed/rep.php?gambar=' + encodeURIComponent(text))
 let samu = await url.json()
 let erest = samu[Math.floor(Math.random() * samu.length)]
-            await conn.sendFile(m.chat, erest, '', 'FUERON DEMACIADAS IMAGENES DE *' + encodeURIComponent(text) + '* TU IMAGEN SE ENCONTRO CON EXITO👌', m)
-
+            await conn.sendFile(m.chat, erest, '', 'Fuente*' + encodeURIComponent(text) + '* TU IMAGEN SE ENCONTRO CON EXITO✅', m)
+            await conn.sendFile(m.chat, erest, '', '_BY:_ : *' ✨𝐁𝐨𝐭 𝐀𝐫𝐫𝐨𝐰✨'*, m)
 {
 
       conn.sendFile(m.chat, 'resultado1.mp3', '', 'xd', m)
