7b2872647113d24d9bb2a4c40727e5fed983a60a
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-06 05:11:49 -0500

    Update zzzzz.js

--- plugins/zzzzz.js
+++ plugins/zzzzz.js
@@ -1,9 +1,9 @@
 let handler = (m, { usedPrefix, command }) => {
 let name = conn.getName(m.sender)
  m.reply(`
-_Hola lo siento_ ${name},,
-El comando *${m.text}*
-_no está registrado en mi sistema porfavor para ver todas mis funciones usa_ ${usedPrefix}menu
+_Hola👋 lo siento_ ${name},,
+_El comando* *${m.text}*
+_no está registrado en mi sistema porfavor para ver todas mis funciones usa_ *${usedPrefix}menu*
 `.trim())
 }
 handler.command = new RegExp
