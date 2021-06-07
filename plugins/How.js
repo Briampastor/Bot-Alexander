ef10d685e7104f33c411b57c394e7dda0393419a
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-07 17:43:26 -0500

    Update How.js

--- plugins/How.js
+++ plugins/How.js
@@ -2,16 +2,16 @@ let handler = async (m, { conn, command, text }) => {
   if (!text) throw `Siapa yang *${command.replace('how', '').toUpperCase()}*`
   conn.reply(m.chat, `
 ${command} *${text}*
-*${text}* is *${Math.floor(Math.random() * 101)}*% ${command.replace('how', '').toUpperCase()}
+*${text}* es *${Math.floor(Math.random() * 101)}*% ${command.replace('how', '').toUpperCase()}
 `.trim(), m, m.mentionedJid ? {
     contextInfo: {
       mentionedJid: m.mentionedJid
     }
   } : {})
 }
-handler.help = ['gay', 'pintar', 'cantik', 'ganteng', 'gabut', 'gila', 'lesbi', 'stress', 'bucin', 'jones', 'sadboy'].map(v => 'how' + v + ' siapa?')
+handler.help = ['gay', 'grilla', 'inteligente', 'guapo', 'bulgar', 'presumido', 'lesbiana', 'estresado', 'puerco', 'meco', 'chicotriste'].map(v => 'how' + v + ' siapa?')
 handler.tags = ['kerang']
-handler.command = /^how(gay|pintar|cantik|ganteng|gabut|gila|lesbi|stress?|bucin|jones|sadboy)/i
+handler.command = /^how(gay|grilla|inteligente|guapo|bulgar|presumido|lesbiana|estresado?|puerco|meco|chicotriste)/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
