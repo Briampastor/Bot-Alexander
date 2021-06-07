d175fbaa392c0f6b73771ccdc39c4959899960b6
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-07 06:06:15 -0500

    Delete Bot.js

--- plugins/Bot.js
+++
@@ -1,7 +0,0 @@
-let handler = m => m.reply('*Si... si estoy aqui.*👻\n\n¿En que te puedo ayudar pavo/a🦃? Escribe *#help* o *#Menú* para que puedas ver mis funciones.')
-
-handler.customPrefix = /Bot|kuhong|hong|Hong/i
-handler.command = new RegExp
-handler.group = true
-
-module.exports = handler
