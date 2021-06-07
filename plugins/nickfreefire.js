e14b9491fe128d19c2f47829f9026ec48db1ffd5
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-06 23:17:23 -0500

    Update nickfreefire.js

--- plugins/nickfreefire.js
+++ plugins/nickfreefire.js
@@ -8,7 +8,7 @@ handler.tags = ['premium']
 handler.command = /^(nickfreefire)$/i
 handler.owner = false
 handler.mods = false
-handler.premium = true
+handler.premium = false
 handler.group = true
 handler.private = false
 
