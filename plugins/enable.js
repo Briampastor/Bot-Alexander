386fac9da0d11d6544b2b8318b5de5be6e68d007
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-07 05:22:21 -0500

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -23,7 +23,7 @@ let handler = async (m, { conn, usedPrefix, command, text, args, isROwner }) => 
       break
     default:
       return m.reply(`
-List option: welcome | delete | public | antilink | antitoxicos | bye
+List option: welcome | delete | public | antilink | antitoxicos | bye | detect
 
 Ejemplo:
 ${usedPrefix}enable welcome
