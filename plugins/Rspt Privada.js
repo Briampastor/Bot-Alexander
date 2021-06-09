9d927feb127ea1d133f97b0f5d736f264b5b938d
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-09 02:23:17 -0500

    Update Rspt Privada.js

--- plugins/Rspt Privada.js
+++ plugins/Rspt Privada.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('*Hola soy un bot🌚*\n\n*lo siento no puedo reponderte pero puedes enviarle un mensaje a mi creador desde este chat con el comando #reportar o el comando #creador y te enviare el numero de mi creador. Que tengas un buen día, bye👋*')
+let handler = m => m.reply('*Hola soy un bot🌚*\n\n*lo siento no puedo reponderte pero puedes enviarle un mensaje a mi creador desde este chat con el comando #reportar y el problema que tengas o el comando #creador y te enviare el numero de mi creador. Que tengas un buen día, bye👋*')
 
 handler.customPrefix = /Hola|Servididor|Server|servers|hola|server|Que|tal|Buenas|noches|días|dias|hp|s|sombra|play|play2/i
 handler.command = new RegExp
