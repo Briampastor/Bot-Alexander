4f3134962d6290135594051206f98c64b09cf755
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-09 20:02:36 -0500

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn, text }) => {
     let [_, code] = text.match(linkRegex) || []
     if (!code) throw '*Link invalido porfavor ingrese un enlace válido*'
     let res = await conn.acceptInvite(code)
-    m.reply(`Berhasil join grup ${res.gid}`)
+    m.reply(`Se unió con éxito al grupo ${res.gid}`)
 }
-handler.help = ['agregarbot [chat.whatsapp.com]']
+handler.help = ['aggbot [chat.whatsapp.com]']
 handler.tags = ['premium']
 
-handler.command = /^agregarbot$/i
+handler.command = /^aggbot$/i
 handler.admin = true
 handler.premium = true
 
