05cc4f1debf1f6394292bd7f3f1e8e5bda20e220
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-09 19:35:30 -0500

    Update Yisus.js

--- plugins/Yisus.js
+++ plugins/Yisus.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('*Es una...*\n\n*loca que le encantan los hombres de color afrodescendiente*')
+let handler = m => m.reply('*Es una...*\n\n*loca que le encantan los pitos y los hombres marrones de África*')
 
 handler.customPrefix = /Yisus|yisus|yisuss|Hong/i
 handler.command = new RegExp
