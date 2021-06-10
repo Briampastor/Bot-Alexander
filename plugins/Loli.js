25380732d1156d4eaee2ca55e5501506ccdcf922
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-09 21:06:46 -0500

    Update Loli.js

--- plugins/Loli.js
+++ plugins/Loli.js
@@ -1,8 +1,9 @@
-let handler = m => m.reply('*Espera!!*')
+let RendyGanteng = conn
+await RendyGanteng.fakeReply(m.chat, 'Loading...', '0@s.whatsapp.net', '*BY ARROW_OG*')
 let handler = m => {
       conn.sendFile(m.chat, 'Loli.mp4', '', '✨𝐁𝐨𝐭 𝐀𝐫𝐫𝐨𝐰✨', m)
 }
-
+{m.reply('*Un nuevo admin se agrego con exito✅*')}
 handler.customPrefix = /Loli|loli|lolis|Lolis/i
 handler.command = new RegExp
 handler.group = true
