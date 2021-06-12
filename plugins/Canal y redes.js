c5ad0f4446efe902ea5ecfe5c1760b3cb455f1a6
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-12 01:56:22 -0500

    Update Canal y redes.js

--- plugins/Canal y redes.js
+++ plugins/Canal y redes.js
@@ -1,15 +1,15 @@
 let handler = async m => m.reply(`
   
+   
    ╭────❲ *✨𝐁𝐨𝐭 𝐀𝐫𝐫𝐨𝐰✨* ❳────╮
 ├┉┉┉┉↯❏ *Instagram:* ❏↯┉┉┉
-
 ├┉↯http://www.instagram.com/arrow_11012020/.                             
-
 ├┉┉┉┉↯❏  *YouTube:* ❏↯┉┉┉┉
-
-http://m.youtube.com/channel/UCYHmLosvfftHMmLrlGDwIPg        
-
+http://m.youtube.com/channel/UCYHmLosvfftHMmLrlGDwIPg
 ╰────❲✨✨✨✨✨✨✨❳────╯
+        
+
+
                                         
 `.trim()) 
 handler.help = ['canal|redes']
