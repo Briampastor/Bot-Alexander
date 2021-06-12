9659ed43d69960d975108728313cef0c666e2f10
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-12 01:11:33 -0500

    Add files via upload

---
+++ plugins/Canal y redes.js
@@ -0,0 +1,19 @@
+let handler = async m => m.reply(`
+  ^    ^    ^    ^    ^  
+ /S\  /m\  /3\  /3\  /0\ 
+<___><___><___><___><___>
+
+*BY:* ✨𝐁𝐨𝐭 𝐀𝐫𝐫𝐨𝐰✨
+ _*Instagram*_
+http://www.instagram.com/arrow_11012020/
+
+𝙈𝙮 𝘾𝙝𝙖𝙣𝙚𝙡:
+
+http://m.youtube.com/channel/UCYHmLosvfftHMmLrlGDwIPg
+                                        
+`.trim()) 
+handler.help = ['Canal|Redes']
+handler.tags = ['info']
+handler.command = /^Canal$/^Redes$i
+
+module.exports = handler
