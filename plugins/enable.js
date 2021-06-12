29331d0764ed7101b124ae6ed741e94f3c30c2e5
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-12 04:58:42 -0500

    Add files via upload

--- plugins/enable.js
+++ plugins/enable.js
@@ -1,14 +1,22 @@
 let handler = async (m, { conn, usedPrefix, command, text, args, isROwner }) => {
   let isEnable = /true|enable|(turn)?on/i.test(command)
   let chat = global.DATABASE._data.chats[m.chat]
+  let user = global.DATABASE._data.users[m.sender]
   let type = (args[0] || '').toLowerCase()
   let isAll = false
+  let isUser = false
   switch (type) {
     case 'welcome':
       chat.welcome = isEnable
       break
+    case 'detect':
+      chat.detect = isEnable
+      break
     case 'delete':
       chat.delete = isEnable
+      break
+    case 'antidelete':
+      chat.delete = !isEnable
       break
     case 'public':
       isAll = true
@@ -21,21 +29,23 @@ let handler = async (m, { conn, usedPrefix, command, text, args, isROwner }) => 
     case 'antilink':
       chat.antiLink = isEnable
       break
+    case 'autolevelup':
+      isUser = true
+      user.autolevelup = isEnable
+      break
     default:
       return m.reply(`
-List option: welcome | delete | public | antilink | antitoxicos | Antitroli | Antispam | antilink2 | autolevelup
-
-──────❲ *✨Ejemplo✨:* ❳───────
-├┉↯ ${usedPrefix}enable welcome
-├┉↯ ${usedPrefix}disable welcome
+List option: welcome | delete | public | antilink | autolevelup | detect
+Ejemplo:
+${usedPrefix}enable welcome
+${usedPrefix}disable welcome
 `.trim())
   }
   m.reply(`
-*${type}* se a *${isEnable ? 'activado' : 'desactivado'}* en ${isAll ? 'este bot' : 'este chat'}
+*${type}* ahora esta *${isEnable ? 'ACTIVO' : 'DESACTIVADO'}* ${isAll ? 'en este bot' : isUser ? '' : 'en este chat'}
 `.trim())
 }
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
-handler.admin = true
 handler.tags = ['group', 'owner']
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff))$/i
 
