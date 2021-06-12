29ed5ea1d0c70762a9708516e5b0e9cd76798f8c
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-12 04:20:59 -0500

    Update eliminar.js

--- plugins/eliminar.js
+++ plugins/eliminar.js
@@ -3,6 +3,7 @@ let handler = async (m, { conn, args }) => {
   let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
+{await m.reply('⏳Procesando⏳')}
 handler.help = ['eliminar','-'].map(v => 'o' + v + ' @user')
 handler.tags = ['owner']
 handler.command = /^(eliminar|o\-)$/i
