093a7765ccfe1222cb288d17ea979b360b1b467e
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-14 21:18:59 -0500

    Update Yisus.js

--- plugins/Yisus.js
+++ plugins/Yisus.js
@@ -1,6 +1,6 @@
 let handler = {
 
-      conn.sendFile(m.chat, 'Estilo1.mp4', '', '*_BY:_* ✨𝐁𝐨𝐭 𝐀𝐫𝐫𝐨𝐰✨', m)
+      conn.sendFile(m.chat, 'Yisusreply.opus', '', '*_BY:_* ✨𝐁𝐨𝐭 𝐀𝐫𝐫𝐨𝐰✨', m)
 
 }
 handler.customPrefix = /Yisus|yisus|yisuss|Hong/i
