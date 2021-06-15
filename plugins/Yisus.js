975e0d3645746126be74c494c23bb18f83769058
ArrowOG <83860888+ArrowOG@users.noreply.github.com>
2021-06-14 21:24:37 -0500

    Delete Yisus.js

--- plugins/Yisus.js
+++
@@ -1,9 +0,0 @@
-let handler = {
-
-      conn.sendFile(m.chat, 'Yisusreply.opus', '', '*_BY:_* ✨𝐁𝐨𝐭 𝐀𝐫𝐫𝐨𝐰✨', m)
-
-}
-handler.customPrefix = /Yisus|yisus|yisuss|Hong/i
-handler.command = new RegExp
-
-module.exports = handler
